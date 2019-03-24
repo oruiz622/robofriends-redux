84e5b113c380f0e8ba60ae5706ce85b62871981e
oruiz622 <oruiz622@gmail.com>
2019-03-24 17:27:21 -0400

    first commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'sincere@april.biz'
+	},
+	{
+		id: 2,
+		name: 'Ervin Howell',
+		usename: 'Antonette',
+		email: 'shanna@melissa.tv'
+	},
+	{
+		id: 3,
+		name: 'Clementine Bauch',
+		username: 'Samantha',
+		email: 'nathan@yesenia.net'
+	},
+	{
+		id: 4,
+		name: 'Patricia Lebsack',
+		username: 'Karianne',
+		email: 'julianne.oconner@kory.org'
+	},
+	{
+		id: 5,
+		name: 'Chelsey Dietrich',
+		username: 'Kamren',
+		email: 'kam@email.com'
+	},
+	{
+		id: 6,
+		name: 'Erin Davis',
+		username: 'Erin',
+		email: 'erin@dav.biz'
+	},
+	{
+		id: 7,
+		name: 'Emily Ruiz',
+		usename: 'Em',
+		email: 'emily@email.com'
+	},
+	{
+		id: 8,
+		name: 'Hunk Howard',
+		username: 'Hunk',
+		email: 'hhoward@email.com'
+	},
+	{
+		id: 9,
+		name: 'Sheila Stacks',
+		username: 'Stax',
+		email: 'shestacks@gmail.com'
+	},
+	{
+		id: 10,
+		name: 'Ron Goldmaine',
+		username: 'GoldMan',
+		email: 'ron@email.com'
+	}
+];
