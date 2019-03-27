b00b6a54599555af4c378ee41c91f33f4b9b0b5c
oruiz622 <oruiz622@gmail.com>
2019-03-27 10:17:03 -0400

    redux store and provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
+import { searchRobots } from './reducers';
 import './index.css';
 import 'tachyons';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import { robots } from './robots';
 
-ReactDOM.render(<App robots={robots} />, document.getElementById('root'));
+const store = createStore(searchRobots);
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
